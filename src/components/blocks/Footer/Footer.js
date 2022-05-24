13cb879b8d9e57480017d04d74b7dd9dbc0e9c65
Sasha <al.pryadchenko@gmail.com>
2022-05-24 16:22:22 +0200

    added files

---
+++ src/components/blocks/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import useTranslation from "../../customHooks/translations";
+
+// import { Link } from "react-router-dom";
+
+function Footer() {
+  // const strings = useTranslation();
+
+  return <footer className="App-footer">footer</footer>;
+}
+
+export default Footer;
---
+++ src/components/blocks/Main/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import useTranslation from "../../customHooks/translations";
+
+// import { Link } from "react-router-dom";
+
+function Main() {
+  // const strings = useTranslation();
+
+  return <main className="App-main">main</main>;
+}
+
+export default Main;
